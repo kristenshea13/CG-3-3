a9bc9fd27f2ac4798fec49a2ef2d5ab28ad1b8f9
Kristen Shea <kstar61@live.com>
2018-07-24 16:39:43 -0500

    statement, expression, operators, and operand definitions and examples

--- CG 3-3/Program.cs
+++ CG 3-3/Program.cs
@@ -6,7 +6,42 @@ namespace CG_3_3
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            //statement - complete "sentence" of code, made up of expression(s)
+            Console.WriteLine("Good Morning!");
+            Console.ReadLine();
+
+            //expression - uses operators and operands
+            int b = 2;
+            //operand
+
+            int c = 1;
+            //operand
+
+            int d = b - c;
+            //expression
+
+            Console.WriteLine(d);
+            Console.ReadLine();
+
+
+            //operator - used for completing action within the code
+            int x = 1;
+            int y = 2;
+            Console.WriteLine(x + y);
+            Console.ReadLine();
+            //operators used = + ( ) ; .
+
+
+            //operand - values (need defining)
+            Console.Write("What is your name? ");
+            string myName = Console.ReadLine();
+            //"myName" is the operand
+
+
+
+
+
+
         }
     }
 }
